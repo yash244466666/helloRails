7efb860eb5d8bf18a6d1dce4ace3ab5b5884acc9
Yash <74345713+yash244466666@users.noreply.github.com>
2023-04-18 19:48:01 +0600

    Add hello world in the app

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  # get 'pages/hello'
+  root 'pages#hello'
+  get 'about' => 'pages#about'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
